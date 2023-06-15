835537d9c2e86410b43879a62b0561a503efd55d
CJ <CJ@THINKPAD>
2023-06-14 21:41:05 -0500

    Basic 2

---
+++ Miscelaneous/Miscelaneous/Utilities.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miscelaneous
+{
+    public static class Utilities
+    {
+        public static List<int> Moda(List<int> numbers) 
+        {
+            // Lista null es una lista de nunca se instancio
+            // Lista Vacia es una lista instanciado.
+            var moda = new List<int>();
+            return moda;
+
+        }
+    }
+}
